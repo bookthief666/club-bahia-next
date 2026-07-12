346dc964780dcdce3984b0e386375a4fc6a81c28
bookthief666 <manuel.orrantia@gmail.com>
2026-07-11 23:39:30 -0700

    feat: add trackable promotion links to publish workflow

--- app/admin/events/[eventId]/publishing/execute/page.tsx
+++ app/admin/events/[eventId]/publishing/execute/page.tsx
@@ -1,4 +1,5 @@
 import { CampaignLaunchClient } from '@/components/admin/publishing/CampaignLaunchClient';
+import { CampaignTrackingLinksClient } from '@/components/admin/publishing/CampaignTrackingLinksClient';
 import { WebsitePublishClient } from '@/components/admin/publishing/WebsitePublishClient';
 import { CampaignWorkflowNav } from '@/components/admin/workflow/CampaignWorkflowNav';
 
@@ -14,6 +15,9 @@ export default async function EventPublishingExecutionPage({
       <CampaignWorkflowNav eventId={eventId} />
       <div className="mb-5">
         <WebsitePublishClient eventId={eventId} />
+      </div>
+      <div className="mb-5">
+        <CampaignTrackingLinksClient eventId={eventId} />
       </div>
       <CampaignLaunchClient eventId={eventId} />
     </>
