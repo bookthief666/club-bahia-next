acf08f8cb9e3d5373b82ed304627fba8f8d28a75
bookthief666 <manuel.orrantia@gmail.com>
2026-07-11 23:15:07 -0700

    feat: add reservation inbox route

---
+++ app/admin/reservations/page.tsx
@@ -0,0 +1,5 @@
+import { ReservationInboxClient } from '@/components/admin/reservations/ReservationInboxClient';
+
+export default function AdminReservationsPage() {
+  return <ReservationInboxClient />;
+}
