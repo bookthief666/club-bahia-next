c0eb04d6fe1154fa6dfc8f434c522f76d21762b9
bookthief666 <manuel.orrantia@gmail.com>
2026-07-15 16:45:21 -0700

    Add platform version builder to media library

--- app/admin/media/page.tsx
+++ app/admin/media/page.tsx
@@ -1,7 +1,13 @@
+import { MediaDerivativeWorkspaceClient } from '@/components/admin/assets/MediaDerivativeWorkspaceClient';
 import { MediaLibraryClient } from '@/components/admin/assets/MediaLibraryClient';
 
 export const dynamic = 'force-dynamic';
 
 export default function AdminMediaLibraryPage() {
-  return <MediaLibraryClient />;
+  return (
+    <>
+      <MediaDerivativeWorkspaceClient />
+      <MediaLibraryClient />
+    </>
+  );
 }
