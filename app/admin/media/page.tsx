310a168f0bae55d6cae66f5733873f6e9aced2d7
bookthief666 <manuel.orrantia@gmail.com>
2026-07-15 15:20:49 -0700

    Add global reusable media library page

---
+++ app/admin/media/page.tsx
@@ -0,0 +1,7 @@
+import { MediaLibraryClient } from '@/components/admin/assets/MediaLibraryClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminMediaLibraryPage() {
+  return <MediaLibraryClient />;
+}
