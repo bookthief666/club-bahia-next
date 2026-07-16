9e53d67b7d122e75775152d554947eef8d344249
bookthief666 <manuel.orrantia@gmail.com>
2026-07-15 19:54:14 -0700

    Add universal promotion review route

---
+++ app/admin/review/page.tsx
@@ -0,0 +1,7 @@
+import { PromotionReviewInboxClient } from '@/components/admin/review/PromotionReviewInboxClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function PromotionReviewInboxPage() {
+  return <PromotionReviewInboxClient />;
+}
