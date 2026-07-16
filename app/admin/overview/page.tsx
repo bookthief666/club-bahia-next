11b00a2bfead3249a9c07440e814e301c91c43c9
bookthief666 <manuel.orrantia@gmail.com>
2026-07-16 14:43:41 -0700

    Add owner overview route

---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,7 @@
+import { OwnerOverview } from '@/components/admin/owner/OwnerOverview';
+
+export const dynamic = 'force-dynamic';
+
+export default function OwnerOverviewPage() {
+  return <OwnerOverview />;
+}
