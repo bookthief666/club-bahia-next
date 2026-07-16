74a9345a29e58b57afcd74fa732a680787365051
bookthief666 <manuel.orrantia@gmail.com>
2026-07-15 17:49:28 -0700

    Add vertical video edit handoff to post preparation

--- app/admin/events/[eventId]/publishing/page.tsx
+++ app/admin/events/[eventId]/publishing/page.tsx
@@ -1,5 +1,6 @@
 import { PreparePostsClient } from '@/components/admin/publishing/PreparePostsClient';
 import { ConversionLinkQuickFixClient } from '@/components/admin/publishing/ConversionLinkQuickFixClient';
+import { VerticalVideoEditReadinessClient } from '@/components/admin/publishing/VerticalVideoEditReadinessClient';
 import { CampaignWorkflowNav } from '@/components/admin/workflow/CampaignWorkflowNav';
 
 export default async function EventPublishingPage({
@@ -13,6 +14,7 @@ export default async function EventPublishingPage({
     <>
       <CampaignWorkflowNav eventId={eventId} />
       <ConversionLinkQuickFixClient eventId={eventId} />
+      <VerticalVideoEditReadinessClient eventId={eventId} />
       <PreparePostsClient eventId={eventId} />
     </>
   );
